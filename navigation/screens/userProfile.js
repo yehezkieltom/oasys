f8f8ce76ad57b34878cfd31a0b7f529368e6a080
Mendriksen <miec00001@stud.uni-saarland.de>
2023-05-27 17:26:44 +0200

    initialize profile

---
+++ navigation/screens/userProfile.js
@@ -0,0 +1,82 @@
+import {Button, Pressable, StyleSheet, Text, TextInput, TouchableHighlight, TouchableOpacity, View} from "react-native";
+import {useState} from "react";
+import homeScreen from "./homeScreen";
+import {Divider, List} from "react-native-paper";
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+
+
+
+
+
+ function userProfile() {
+    return (
+     <View style={styles.wholeScreen}>
+        <View style={styles.logoScreen}>
+            <Text style={styles.titleOasys} >oasys</Text>
+        </View>
+        <View style={styles.buttonS}>
+            <Divider bold/>
+            <View>
+                <List.Item onPress={() => console.log('App Settings pressed!')}
+                           titleStyle={styles.textAppHealth} title="App Settings"
+                           left={(props) => <Icon {...props} size={35} name="cog" />}/>
+            </View>
+            <Divider bold/>
+            <View>
+                <List.Item onPress={() => console.log('Health Details pressed!')}
+                           titleStyle={styles.textAppHealth} title="Health Details"
+                           left={(props) => <Icon {...props} size={35} name="clipboard-pulse" /> }/>
+            </View>
+            <Divider bold/>
+
+
+
+        </View>
+     </View>
+    )
+}
+
+export default userProfile;
+const styles= StyleSheet.create ( {
+
+    textAppHealth: {
+      color: 'black',
+      fontSize: 24,
+      paddingBottom: 5,
+      paddingTop: 5,
+      paddingLeft: 5,
+    },
+
+   buttonS:{
+     justifyContent: "flex-start",
+     flexDirection:  "column",
+
+   },
+
+   wholeScreen: {
+        flex: 1,
+   },
+
+   logoScreen: {
+       backgroundColor: 'white',
+       alignItems: 'center',
+       marginTop:45,
+       paddingBottom: 15,
+   },
+
+    titleOasys: {
+       color: 'black',
+       fontSize: 24,
+       fontWeight: 600,
+    },
+
+    textInput: {
+        borderWidth: 1,
+        borderColor: '#cccccc',
+        width: '70%',
+        marginRight: 8,
+        padding: 8
+    },
+});
+
